ceffd579699068b538d231304fd626a65f30b9a2
Artem <kolomytsev1996@gmail.com>
2020-03-14 00:50:58 +0200

    implement basic concurrent quick sort

--- chapter4/quicksort.go
+++ chapter4/quicksort.go
@@ -1,6 +1,12 @@
 package quicksort
 
-import "math/rand"
+import (
+	"log"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
 
 func Sort(a []int) []int {
 	if len(a) < 2 {
@@ -26,4 +32,67 @@ func Sort(a []int) []int {
 	Sort(a[left+1:])
 
 	return a
+}
+
+type ConcurrentSorter struct {
+	jobs    chan []int
+	counter atomic.Value
+	wg      sync.WaitGroup
+}
+
+func ConcurrentSort(a []int) []int {
+	sorter := &ConcurrentSorter{
+		jobs: make(chan []int, 1000),
+		wg:   sync.WaitGroup{},
+	}
+	for i := 0; i < 4; i++ {
+		go sorter.Worker()
+	}
+
+	sorter.wg.Add(1)
+	sorter.jobs <- a
+	sorter.wg.Wait()
+
+	return a
+}
+func (sorter *ConcurrentSorter) Worker() {
+	func(jobs chan []int) {
+		for {
+			select {
+			case toSort := <-jobs:
+				sortChunk(toSort, jobs, &sorter.wg)
+			case <-time.After(100 * time.Second):
+				log.Fatal("failed")
+				return
+			}
+		}
+	}(sorter.jobs)
+}
+
+func sortChunk(a []int, jobs chan<- []int, wg *sync.WaitGroup) {
+	if len(a) < 2 {
+		wg.Done()
+		return
+	}
+
+	left, right := 0, len(a)-1
+
+	pivot := rand.Int() % len(a)
+
+	a[pivot], a[right] = a[right], a[pivot]
+
+	for i, _ := range a {
+		if a[i] < a[right] {
+			a[left], a[i] = a[i], a[left]
+			left++
+		}
+	}
+
+	a[left], a[right] = a[right], a[left]
+
+	wg.Add(2)
+	jobs <- a[:left]
+	jobs <- a[left+1:]
+
+	wg.Done()
 }
--- chapter4/quicksort_test.go
+++ chapter4/quicksort_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestSort(t *testing.T) {
 	src := []int{11, 12, 0, -1, math.MinInt64, math.MaxInt64, 10, 2, 2, 2}
-	result := Sort(src)
+	result := ConcurrentSort(src)
 
 	expected := []int{math.MinInt64, -1, 0, 2, 2, 2, 10, 11, 12, math.MaxInt64}
 	for i := range expected {
@@ -20,7 +20,7 @@ func TestSort(t *testing.T) {
 
 func Test_Sort(t *testing.T) {
 	src := []int{4, 3, 1, 5, 2}
-	result := Sort(src)
+	result := ConcurrentSort(src)
 
 	expected := []int{1, 2, 3, 4, 5}
 	for i := range expected {
